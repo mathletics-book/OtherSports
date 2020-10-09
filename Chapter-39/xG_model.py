f55180b99c4684eae0e4286fb0c0ebfdfd7d34b7
mathletics-book <42146212+mathletics-book@users.noreply.github.com>
2020-10-08 23:34:43 -0400

    Update xG_model.py

--- Chapter-39/xG_model.py
+++ Chapter-39/xG_model.py
@@ -1,1 +1,9 @@
+import statsmodels.formula.api as smf
+import pandas as pd 
+import numpy as np
 
+mls = pd.read_csv("MLSshots.csv")
+f = 'Outcome~x + y + C(Assist) + C(Type) + C(Play) + Angle + Distance'
+
+xG_mod = smf.glm(formula = f, data = mls, family = sm.families.Binomial()).fit()
+print(logitfit.summary())
