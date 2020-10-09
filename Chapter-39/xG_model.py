6ac12d1cc5cd90f453e82ace368f397a81302b52
mathletics-book <42146212+mathletics-book@users.noreply.github.com>
2020-10-08 23:53:01 -0400

    added calibration curve

--- Chapter-39/xG_model.py
+++ Chapter-39/xG_model.py
@@ -1,9 +1,29 @@
 import statsmodels.formula.api as smf
+from sklearn.calibration import calibration_curve
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 import pandas as pd 
 import numpy as np
 
 mls = pd.read_csv("MLSshots.csv")
 f = 'Outcome~x + y + C(Assist) + C(Type) + C(Play) + Angle + Distance'
 
-xG_mod = smf.glm(formula = f, data = mls, family = sm.families.Binomial()).fit()
+X_train, X_test, y_train, y_test = train_test_split(mls, mls['Outcome'], test_size=0.2)
+xG_mod = smf.glm(formula = f, data = X_train, family = sm.families.Binomial()).fit()
 print(logitfit.summary())
+
+
+## this computes the calibration curve out-of-sample
+
+observed, predicted = calibration_curve(X_test['Outcome'], logitfit.predict(X_test), n_bins = 10)
+
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+x = np.linspace(0,1,10000)
+y = x
+ax1.plot(x,y,'-.g',label="Perfect Calibration")
+ax1.scatter(predicted,observed,s=20, c='b', marker="s", label = "xG Model")
+plt.xlabel("Predicted Shot Make Probability")
+plt.ylabel("Observed Shot Make Probability")
+plt.legend(loc='upper left')
+plt.show()
